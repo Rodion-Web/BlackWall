2df7aa941e53845b35a586d9b86c0e0b1dd6e5e7
Rodion-Web <rodeus770@gmail.com>
2021-08-24 12:57:29 +0300

    added all files BlackWall

---
+++ js/metting.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector(".menu-button");
+  const menu  = document.querySelector(".navbar-panel")
+  const menuCloseBtn = document.querySelector(".close-menu");
+  menuBtn.addEventListener("click", () => {
+      menu.classList.toggle("is-open");
+  });
+  menuCloseBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+});
